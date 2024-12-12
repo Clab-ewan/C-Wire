95f063786f241656befc672c315b41d6757d8cf2
paulmarmelat <marmelatpa@cy-tech.fr>
2024-12-12 09:13:17 +0000

    changement de malade

--- codeC/main.c
+++ codeC/main.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 #include "avl_tree.h"
 
+// Function main to read the data from the standard input and insert it in the AVL tree 
 
 int main(){
     AVLNode *root = NULL;
-    int station_id = 0;
+    int station_id = 0;// initialize the data of the station
     int h = 0;
     long load = 0, capacity = 0;
-    while (scanf("%d;%ld;%ld\n", &station_id, &capacity, &load) != EOF) {
+    while (scanf("%d;%ld;%ld\n", &station_id, &capacity, &load) != EOF) {// Read the data from the standard input
         root = insertAVL(root, station_id, capacity, load, &h);
     }
     printf("Station_id;capacity;load\n");
-    inorder(root);
+    inorder(root); // Display the AVL tree in infix order
     root = freeAVL(root);
     return 0;
 }
